b3a541f5227ebae78bf9880b77e75eca1fa75017
Nathan Gage <ngage@smu.edu>
2021-09-08 19:24:24 -0500

    Add final results screen

--- MemeMatchup/ResultsViewController.swift
+++ MemeMatchup/ResultsViewController.swift
@@ -8,15 +8,71 @@
 import UIKit
 
 class ResultsViewController: UIViewController {
-
+    
+    @IBOutlet weak var firstPlaceLabel: UILabel!
+    
+    @IBOutlet weak var secondPlaceLabel: UILabel!
+    
+    @IBOutlet weak var thirdPlaceLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        print("I AM HERE")
-
-        // Do any additional setup after loading the view.
+        
+        let players = MemeRoundsModel.shared.getPlayers()
+        var playerScores: Dictionary<String, Int> = [:]
+        
+        for player in players
+        {
+            playerScores[player, default: 0] = MemeRoundsModel.shared.getRoundWinsFor(player: player)
+        }
+        
+        let sortedScores = playerScores.sorted { (first, second) -> Bool in
+            return first.value > second.value
+        }
+        
+        if let first = sortedScores[safe: 0]
+        {
+            if first.value > 1
+            {
+                firstPlaceLabel.text = "👑 \(first.key) - \(first.value) Votes"
+            }
+            else {
+                firstPlaceLabel.text = "👑 \(first.key) - \(first.value) Vote"
+            }
+        }
+        
+        if let second = sortedScores[safe: 1]
+        {
+            if second.value > 1
+            {
+                secondPlaceLabel.text = "\(second.key) - \(second.value) Votes"
+            }
+            else {
+                secondPlaceLabel.text = "👑 \(second.key) - \(second.value) Vote"
+            }
+        }
+        
+        if let third = sortedScores[safe: 2]
+        {
+            if third.value > 1
+            {
+                thirdPlaceLabel.text = "\(third.key) - \(third.value) Votes"
+            }
+            else {
+                thirdPlaceLabel.text = "👑 \(third.key) - \(third.value) Vote"
+            }
+        }
     }
     
-
+    
+    @IBAction func onBackToHome(_ sender: Any) {
+        let storyBoard : UIStoryboard = UIStoryboard(name: "Main", bundle:nil)
+        
+        let nextViewController = storyBoard.instantiateViewController(withIdentifier: "HomeViewController") as! HomeViewController
+        
+        self.present(nextViewController, animated:true, completion:nil)
+    }
+    
     /*
     // MARK: - Navigation
 
@@ -27,4 +83,13 @@ class ResultsViewController: UIViewController {
     }
     */
 
+}
+
+// https://stackoverflow.com/a/30593673/13009703
+extension Collection {
+
+    /// Returns the element at the specified index if it is within bounds, otherwise nil.
+    subscript (safe index: Index) -> Element? {
+        return indices.contains(index) ? self[index] : nil
+    }
 }
