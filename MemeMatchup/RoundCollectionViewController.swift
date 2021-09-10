30ec99d23193278b876aae12fe85c183e9a17900
Edward-jiang495 <54651927+Edward-jiang495@users.noreply.github.com>
2021-09-09 19:25:54 -0500

    label misplaced error fixed

--- MemeMatchup/RoundCollectionViewController.swift
+++ MemeMatchup/RoundCollectionViewController.swift
@@ -3,11 +3,6 @@
 //  MemeMatchup
 //
 //  Created by Zhengran Jiang on 9/6/21.
-//question ask:
-
-//current bugs:
-//multiple rounds go out of bounds at ind
-//declare winner abnormal
 
 import UIKit
 
@@ -15,20 +10,16 @@ private let reuseIdentifier = "cellId"
 private var ind = 0;
 class RoundCollectionViewController: UICollectionViewController, UICollectionViewDelegateFlowLayout {
     @IBAction func winClick(_ sender: UIButton) {
-        print("win selected");
+        
         let cell = sender.nearestAncestor(ofType: UICollectionViewCell.self);
-        
         let indexPath = self.collectionView?.indexPath(for: cell!);
-        print(indexPath);
-        
         var playernames = MemeRoundsModel.shared.getPlayers()
         var player = playernames[indexPath!.row]
         MemeRoundsModel.shared.declareWinner(winner: player)
-//        let name = MemeRoundsModel.shared.getCaptionedMemesForRound(round: currRound)[indexPath];
-//        MemeRoundsModel.shared.declareWinner(winner: name)
-        print("rounds")
-        print(MemeRoundsModel.shared.currentRound)
-        print(MemeRoundsModel.shared.numOfRounds)
+        print("Winner: ")
+        print(indexPath)
+//        print(MemeRoundsModel.shared.currentRound)
+//        print(MemeRoundsModel.shared.numOfRounds)
         if(MemeRoundsModel.shared.currentRound > MemeRoundsModel.shared.numOfRounds){
             //if we go through all rounds
             if let vc = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "ResultView") as? ResultsViewController
@@ -36,22 +27,13 @@ class RoundCollectionViewController: UICollectionViewController, UICollectionVie
                 if let nav = navigationController{
                     nav.pushViewController(vc, animated: true)
                 }
-    //            self.present(vc, animated: true, completion: nil)
         
             }
-            print("JERE")
+            
         }
         else{
             if let vc = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "RoundStart") as? RoundStartViewController
             {
-//                MemeRoundsModel.shared.incRound();
-//                increase round by 1
-//                vc.currRound = currRound + 1;
-//                vc.round_count = numRounds;
-//                vc.players = ["AAA","AAB"]
-//                vc players = all player name in memes
-//                we need to enter info again since its cleaned
-                //round goes up
             
                 if let nav = navigationController{
                     nav.pushViewController(vc, animated: true)
@@ -59,13 +41,9 @@ class RoundCollectionViewController: UICollectionViewController, UICollectionVie
     //            self.present(vc, animated: true, completion: nil)
         
             }
-            print("HERE")
+            
         }
     }
-//    var scr = UIScrollView();
-//    override func viewForZooming(in scrollView: UIScrollView) -> UIView? {
-//        return self.image
-//    }
 
     let cellId = "cellId";
     
@@ -156,9 +134,13 @@ class RoundCollectionViewController: UICollectionViewController, UICollectionVie
         // Set positions
         imageView.frame = CGRect(x: 0, y: 0, width: view.frame.width, height: view.frame.height)
         
-        topLabel.frame = CGRect(x:view.frame.width/2, y: view.frame.height/8, width: imageView.frame.size.width, height:20)
+        topLabel.frame = CGRect(x: (view.frame.width/2), y: (view.frame.height/8), width: imageView.frame.size.width, height:80)
+        topLabel.center.x = view.center.x
+        topLabel.center.y = view.center.y/4
         
-        bottomLabel.frame = CGRect(x:view.frame.width/2, y: view.frame.height*3/4, width: imageView.frame.size.width, height:20)
+        bottomLabel.frame = CGRect(x: (view.frame.width/2), y: (view.frame.height*3/4), width: imageView.frame.size.width, height:80)
+        bottomLabel.center.x = view.center.x
+        bottomLabel.center.y = view.center.y * 1.5
         
         // Add everything to cell
         imageView.addSubview(topLabel)
@@ -168,65 +150,6 @@ class RoundCollectionViewController: UICollectionViewController, UICollectionVie
         
         return cell
         
-        // OLD CODE
-        /*if (ind < MemeRoundsModel.shared.getPlayers().count){
-            
-            print("sHARED")
-            //            print(MemeRoundsModel.shared.getPlayers())
-            //            var dict = MemeRoundsModel.shared.getCaptionedMemesForRound(round: MemeRoundsModel.shared.currentRound-1);
-            var dict = MemeRoundsModel.shared.getCaptionedMemesForRound(round: ind);
-            //            current round start at 1 so -1
-            var playernames = MemeRoundsModel.shared.getPlayers()
-            
-            var player = playernames[ind]
-            print(dict[player]?.meme_url)
-            
-            //            var img = arr[ind];
-            print("Dict")
-            print(dict)
-            print("IND")
-            print(ind)
-            print("player")
-            print(player)
-            var imageName = ""
-            imageName = dict[player]!.meme_url;
-            print(imageName)
-            ind = ind+1
-            var url = URL(string: imageName)
-            
-            if let data = try? Data(contentsOf: url!)
-            {
-                let image: UIImage = UIImage(data: data)!
-                let imageView = UIImageView(image: image)
-                imageView.frame = CGRect(x: 0, y: 0, width: view.frame.width, height: view.frame.height)
-                var topLabel = UILabel(frame: CGRect(x:view.frame.width/2, y: view.frame.height/8, width: imageView.frame.size.width, height:20))
-                topLabel = dict[player]!.topLabel
-                //            topLabel.backgroundColor = UIColor.white
-                imageView.addSubview(topLabel);
-                
-                var botLabel = UILabel(frame: CGRect(x:view.frame.width/2, y: view.frame.height*3/4, width: imageView.frame.size.width, height:20))
-                botLabel = dict[player]!.bottomLabel
-                //            topLabel.backgroundColor = UIColor.white
-                imageView.addSubview(botLabel);
-                cell.addSubview(imageView)
-            }
-        }
-        
-        let winBtn = UIButton(frame: CGRect(x:0,y:20,width: 70,height: 50));
-        winBtn.backgroundColor = UIColor(red: 2/255, green: 117/255, blue: 216/255, alpha: 1)
-
-        winBtn.layer.cornerRadius = 5;
-        winBtn.layer.borderWidth = 1;
-    
-        winBtn.setTitle("win", for: .normal)
-        winBtn.titleLabel?.textColor = UIColor.white;
-        
-        winBtn.addTarget(self, action: #selector(winClick(_:)), for: UIControl.Event.touchUpInside)
-        
-        cell.addSubview(winBtn);
-        
-//        cell.addSubview(roundLabel);
-        return cell;*/
     }
 
 //    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
